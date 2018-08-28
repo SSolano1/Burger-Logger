066780eb221adcd862a5f22f54ef9796f45965b0
Solon Solano <ssolano1@msn.com>
2018-08-27 23:56:55 -0400

    initial

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Drunken Monk', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Kobe Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Waffle Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Pig', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cronie Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('OMG Burger', false);
---
+++ server.js
@@ -0,0 +1,30 @@
+
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// var db = require("./models");
+
+// Static directory
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgerControllers.js");
+
+app.use(routes);
+ 
+
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+
